a32674388f4ea0c5f99f08cb73272a7c68d22487
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-11-25 22:45:39 +0500

    carousal in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavScrollExample from'./componenets/Navbar';
+import NavScrollExample from'./componenet/Navbar';
+import DarkVariantExample from './componenet/carousal';
 function App() {
   return (
     <div>
       
       <NavScrollExample/>
-      
+      <DarkVariantExample/>
 
         </div>
   );
